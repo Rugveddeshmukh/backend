096fbd4ab9c6a0316c206402a851869dfac38eb5
Rugved Deshmukh <rugveddeshmukh00@gmail.com>
2025-08-16 11:51:29 +0530

    first commit

--- server.js
+++ server.js
@@ -36,5 +36,5 @@ app.use('/api/support', supportRoutes);
 app.use('/api/admin', adminRoutes);
 
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
